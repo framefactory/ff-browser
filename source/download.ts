2ed538e2ea236e9a78aea9756b2687718398f92a
Ralph Wiedemeier <ralph@framefactory.ch>
2019-03-27 21:44:02 +0100

    updated documentation

--- source/download.ts
+++ source/download.ts
@@ -17,11 +17,22 @@ const _triggerDownload = function(dataURL: string, fileName: string)
 };
 
 export default {
+    /**
+     * Prompts the user to download the provided text content as a file.
+     * @param text text string to include.
+     * @param fileName name of the downloadable file.
+     */
     text: function(text: string, fileName: string) {
 
         const dataURL = window.URL.createObjectURL(new Blob([text], { type: "text/plain" }));
         _triggerDownload(dataURL, fileName);
     },
+
+    /**
+     * Prompts the user to download the provided JSON content as a file.
+     * @param json JSON data to include.
+     * @param fileName name of the downloadable file.
+     */
     json: function(json: object | string, fileName: string) {
 
         if (typeof json === "object") {
@@ -31,6 +42,12 @@ export default {
         const dataURL = window.URL.createObjectURL(new Blob([json], { type: "text/json" }));
         _triggerDownload(dataURL, fileName);
     },
+
+    /**
+     * Prompts the user to download the content from the given URL.
+     * @param url URL of the content to be downloaded.
+     * @param fileName name of the downloadable file.
+     */
     url: function(url: string, fileName: string) {
         _triggerDownload(url, fileName);
     }
