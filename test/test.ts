781140ea930275f3db1d02f4e1459db94f1aab4f
Ralph Wiedemeier <ralph@framefactory.ch>
2020-05-01 16:42:58 +0200

    updated configuration

--- test/test.ts
+++ test/test.ts
@@ -5,8 +5,21 @@
  * License: MIT
  */
 
+import * as path from "path";
+import * as moduleAlias from "module-alias";
+
+moduleAlias.addAliases({
+    "@ff/browser": path.resolve(__dirname, "../export"),
+    "@ff/core": path.resolve(__dirname, "../../../ff-core/built/export"),
+});
+
+// define vars on node global object (usually done by Webpack)
+global["ENV_DEVELOPMENT"] = false;
+global["ENV_PRODUCTION"] = true;
+global["ENV_VERSION"] = "Test";
+
 ////////////////////////////////////////////////////////////////////////////////
 
 suite("FF Browser", function() {
-    // No tests
+    // no tests
 });
