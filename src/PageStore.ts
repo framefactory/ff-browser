abb18c0cfe816ac7e9ced938cbe91ae545562eab
Ralph Wiedemeier <ralph@framefactory.ch>
2023-08-04 23:29:19 +0200

    add PageStore

---
+++ src/PageStore.ts
@@ -0,0 +1,42 @@
+/**
+ * FF Typescript Foundation Library
+ * Copyright 2023 Ralph Wiedemeier, Frame Factory GmbH
+ *
+ * License: MIT
+ */
+
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Uses browser local storage to store and retrieve persistent data
+ * for a specific page, designated by its url path.
+ */
+export class PageStore<T=any>
+{
+    readonly storage: Storage;
+    readonly path: string;
+    readonly preset: T;
+
+    constructor(path: string, preset: T)
+    {
+        this.storage = window?.localStorage;
+        this.path = path;
+        this.preset = preset;
+    }
+
+    isSupported(): boolean
+    {
+        return !!this.storage;
+    }
+
+    get(): T
+    {
+        const item = this.storage?.getItem(this.path);
+        return item ? JSON.parse(item) : this.preset;
+    }
+
+    set(obj: T): void
+    {
+        this.storage?.setItem(this.path, JSON.stringify(obj));
+    }
+}
